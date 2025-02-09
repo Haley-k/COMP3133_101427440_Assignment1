ec289b7402e82278a526bc3667ef5aff62c1e8f0
Haley <hs.haleyk@gmail.com>
2025-02-09 12:20:47 -0500

    add model

---
+++ model/Employee.js
@@ -0,0 +1,64 @@
+const { validate } = require("graphql");
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      default: "Other",
+    },
+    designation: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+      min: 1000,
+    },
+    date_of_joining: {
+      type: Date,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    employee_photo: {
+      type: String,
+      default: "",
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    collection: "employees",
+  }
+);
+
+employeeSchema.pre("save", function (next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
---
+++ model/User.js
@@ -0,0 +1,42 @@
+const { validate } = require("graphql");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    collection: "users",
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+module.exports = mongoose.model("user", userSchema);
